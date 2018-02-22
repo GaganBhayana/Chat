9c31fd8bede5f6c7c646a9afd62b3170e25e0a4f
GaganBhayana <gagan2oct1998@gmail.com>
2018-02-22 18:34:26 +0530

    commit 3

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
-app =express(); 
+
 
 server = require('http').createServer(app);
 io = require('socket.io').listen(server);
 usernames= [];
+app =express(); 
 
 server.listen(process.env.PORT || 3000,function(){
 	console.log("server Started!");
